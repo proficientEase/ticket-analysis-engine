027a96d8609d86b3e1e57f2f84c7dce6a5aea772
proficientEase <proficientEase@proton.me>
2026-06-11 11:29:51 -0700

    Add table structure to schema.sql and structure_table() to ticket_system.py

---
+++ ticket_system.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+# Establish connection to or create 'support_center.db' if doesnt exist
+# Cursor is used to run SQL commands
+connection = sqlite3.connect('suport_center.db')
+cursor = connection.cursor()
+
+
+# Get table structure from 'schema.sql' and apply it to 'support_center.db'
+def structure_table():
+    try: 
+        # Read the 'schema.sql' file and store as sql_script
+        # Data from the file is stored as plain string
+        with open('schema.sql', 'r') as file:
+            #Read File
+            sql_script = file.read()
+
+            # Use cursor.executescript() instead of .execute() to parse multiple lines of SQL
+            cursor.executescript(sql_script)
+        
+        # Apply the table layout to 'support_center.db'
+        connection.commit()
+    except:
+
+        # Error message
+        print("File not found or unreadable")
+    finally:
+
+        # Close connections for safety
+        cursor.close()
+        connection.close()
